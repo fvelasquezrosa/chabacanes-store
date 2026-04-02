a34128fefa3e802c514b8845d8a49e7897591b09
fvelasquezrosa <fvelasquezrosa@rrc.ca>
2026-04-02 11:21:30 -0500

    Create category and product models with associations and validations

---
+++ app/models/product.rb
@@ -0,0 +1,10 @@
+class Product < ApplicationRecord
+  belongs_to :category
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :category, presence: true
+end
