b894e8135ab7760367effcd8e5e07432526a34ba
fvelasquezrosa <fvelasquezrosa@rrc.ca>
2026-04-02 11:31:52 -0500

    Add category and product management to ActiveAdmin

---
+++ app/admin/products.rb
@@ -0,0 +1,48 @@
+ActiveAdmin.register Product do
+  permit_params :title, :description, :price, :stock_quantity, :category_id, :image
+
+  config.filters = false
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :category
+    column :price
+    column :stock_quantity
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description
+      f.input :price
+      f.input :stock_quantity
+      f.input :category
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :title
+      row :description
+      row :price
+      row :stock_quantity
+      row :category
+      row :created_at
+      row :updated_at
+      row :image do |product|
+        if product.image.attached?
+          image_tag url_for(product.image), style: "max-width: 200px;"
+        else
+          "No image uploaded"
+        end
+      end
+    end
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,12 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :category, presence: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    ["category_id", "created_at", "description", "id", "price", "stock_quantity", "title", "updated_at"]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    ["category"]
+  end
 end
