b7399ddfd517cdd083bdde0512c480403146994c
fvelasquezrosa <fvelasquezrosa@rrc.ca>
2026-04-02 12:48:54 -0500

    Fix cart routing to use singular cart controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
-  resource :cart, only: [:show]
+  resource :cart, only: [:show], controller: "cart"
   post "cart/add/:id", to: "cart#add", as: "add_to_cart"
 end
