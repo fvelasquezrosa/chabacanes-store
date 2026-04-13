3c5fdc22027cee74ac78b6c3fcc475022409e258
fvelasquezrosa <fvelasquezrosa@rrc.ca>
2026-04-13 11:16:19 -0500

    Update schema after making province optional for user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_13_152814) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_13_161012) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -152,7 +152,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_13_152814) do
     t.string "address"
     t.string "city"
     t.string "postal_code"
-    t.integer "province_id", null: false
+    t.integer "province_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
