1bb2435d3ddedd1250033a06d230bceaa43ac36e
fvelasquezrosa <fvelasquezrosa@rrc.ca>
2026-04-13 10:31:16 -0500

    Add provinces, orders, order items, and checkout fields for users

---
+++ db/migrate/20260413152814_add_checkout_fields_to_users.rb
@@ -0,0 +1,10 @@
+class AddCheckoutFieldsToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+    add_column :users, :postal_code, :string
+    add_reference :users, :province, null: false, foreign_key: true
+  end
+end
